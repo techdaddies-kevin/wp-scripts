b25459adaa3802dd2d82023ff2d708248bb186b3
Kevin <kevin@techdaddies.com>
2023-07-12 07:10:44 -0400

    Create multisite-plugins.sh

---
+++ multisite-plugins.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sites=$(wp --skip-plugins --skip-themes --quiet site list --field=url 2> /dev/null)
+plugins=$(wp --skip-plugins --skip-themes  --quiet plugin list --field=name 2> /dev/null)
+for plugin in $plugins; do
+  echo -n "${plugin},;
+
+  for url in $sites; do
+     wp --skip-plugins --skip-themes --quiet plugin is-active "${plugin}" --url="${url}" 2> /dev/null
+     if [[ $? -eq 0 ]]; then
+
+        echo -n "${url} | "
+        echo -ne "\n"
+
+     fi
+  done
+
+done
