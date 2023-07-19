c08023cc9cf60b0c08e756934b62934fb6a7c401
Kevin <kevin@techdaddies.com>
2023-07-19 12:38:05 -0400

    Update multisite-plugins.sh

--- multisite-plugins.sh
+++ multisite-plugins.sh
@@ -3,16 +3,17 @@
 sites=$(wp --skip-plugins --skip-themes --quiet site list --field=url 2> /dev/null)
 plugins=$(wp --skip-plugins --skip-themes  --quiet plugin list --field=name 2> /dev/null)
 for plugin in $plugins; do
-  echo -n "${plugin},;
+  echo -n "${plugin},"
 
   for url in $sites; do
      wp --skip-plugins --skip-themes --quiet plugin is-active "${plugin}" --url="${url}" 2> /dev/null
      if [[ $? -eq 0 ]]; then
 
         echo -n "${url} | "
-        echo -ne "\n"
 
      fi
   done
+
+  echo -ne "\n"
 
 done
